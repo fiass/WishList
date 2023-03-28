19229dd7712fe03ad67cbdbb52bb69726c355aaf
Fiass <roma.sokol200219@gmail.com>
2023-03-28 16:49:41 +0500

    Animation Has Been Added

--- Tabs/Tech.js
+++ Tabs/Tech.js
@@ -47,4 +47,28 @@ document.write(`
 </div>
 <p class="mb-4"></p>
 
+<div class="card">
+  <div class="card-content">
+    <p class="title">
+     6.1" Смартфон Apple iPhone 14 Pro 256 ГБ фиолетовый
+    </p>
+    <p class="subtitle">
+      ядер - 6, 2 SIM, OLED, 2556x1179, камера 48+12+12 Мп, NFC, 5G, GPS
+    </p>
+  </div>
+  <footer class="card-footer">
+    <p class="card-footer-item">
+      <span>
+        Цена на момент поста: <r class="price">118 699</r> ₽
+      </span>
+    </p>
+    <p class="card-footer-item">
+      <span>
+        Узнать цену сейчас: <a href="https://www.dns-shop.ru/product/41472a7f3012ed20/61-smartfon-apple-iphone-14-pro-256-gb-fioletovyj/">DNS</a>
+      </span>
+    </p>
+  </footer>
+</div>
+<p class="mb-4"></p>
+
 `);
