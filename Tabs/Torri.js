861ddb588c6aaa5d540c75d1ebac39c780a5efc1
Fiass <roma.sokol200219@gmail.com>
2023-05-02 15:01:20 +0500

    Torri

--- Tabs/Torri.js
+++ Tabs/Torri.js
@@ -1,25 +1,219 @@
 document.write(`
-  <div class="card">
-  <div class="card-content">
-    <p class="title">
-     Яндекс Плюс
-    </p>
-    <p class="subtitle">
-      Кинопоиск, Музыка и кешбэк баллами в сервисах Яндекса
-    </p>
-  </div>
-  <footer class="card-footer">
-    <p class="card-footer-item">
-      <span>
-        Цена на момент поста: <r class="price"><strong>99</strong></r> ₽
-      </span>
-    </p>
-    <p class="card-footer-item">
-      <span>
-        Узнать цену сейчас: <a href="https://plus.yandex.ru/">PLUS</a>
-      </span>
-    </p>
-  </footer>
-</div>
-<p class="mb-4"></p>
-`);
+  <div class="card"><div class="card-content"><p class="title">
+
+  Розовый игрушечный пистолетик 
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="https://aliexpress.ru/item/1005004030901276.html?spm=a2g2w.cart.cart_split.92.1dcc4aa6YQnMWQ&_ga=2.136756214.775005020.1682966617-1282380947.1665438502&sku_id=12000027801143311">тык</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Тапочки с почито
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="https://aliexpress.ru/item/1005004858098498.html?spm=a2g2w.cart.cart_split.107.1dcc4aa6YQnMWQ&_ga=2.170794214.775005020.1682966617-1282380947.1665438502&sku_id=12000030773590324">тык</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Жидкие блёстки для глаз «Chupa Chups»
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="https://goldapple.ru/11791-19000099080-bling-bling-eyes-duochrome#sku=19000099080">тык</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Духи «Moschino Toy 2 Bubble Gum»
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="https://market.yandex.ru/product--moschino-toy-2-bubble-gum-tualetnaia-voda-30ml/1771380632?clid=1601&utm_source=yandex&utm_medium=search&utm_campaign=ymp_offer_dp_krasota_split_bko_dyb_search_rus&utm_term=15927546%7C101827153730&utm_content=cid%3A75590941%7Cgid%3A4949023281%7Caid%3A12319829435%7Cph%3A2589157%7Cpt%3Apremium%7Cpn%3A2%7Csrc%3Anone%7Cst%3Asearch%7Crid%3A2589157%7Ccgcid%3A0&sku=101827153730&cpa=1">тык</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Палетка теней «Vivienne Sabo»
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="https://market.yandex.ru/product--vivienne-sabo-paletka-tenei-metamourphoses/1454278214?glfilter=14871214%3A27508890_101475120515&cpa=1&cpc=FHBvqJ-Xzgvz11W7LVpdPv9j4YJ-mTfopi7_z785ny8tSbn3-JyAiolbZIbI3kuxycJN5Igm61BwsVjSSE2I58MfBWlMrddRrsY5EKCO4jh9LzN4CenSXSUBnKms_iBQ3mlSkMQLfP4hbk7wz013iYqiJexY6hGBtWtXWDGjT2Y0bULrXn3JhABScaO-Hyd0dbv0O33sLwWdNqpAwLHouNEaeg_GtUXD70Y0-9rFyKafNAChaQpol9rnnbnh7j6vzFnYO1HehFjEIjeUA3sB3w%2C%2C&sku=101475120515&offerid=RfnPWYLQPfD0ohP8XR95qw">тык</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Аниме фигурка из «no game no life»
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="https://sl.aliexpress.ru/p?key=ZHnQwdM">тык</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Аниме фигурка «NieR: Automata 2B»
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Блески и масла для губ
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Чехольчики на телефонб
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Абсолютно любые кастомные вещи своими ручками 
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Картина по номерам 
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Миленькое нижнее бельё
+
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Платье на свой вкус
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Юбочки 
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Спелое манго 
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+  Наборчик из киндеров 
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+  `);
