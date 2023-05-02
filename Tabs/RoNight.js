a2b6df52191d9a48bbadb650a533535a85255d5f
Fiass <roma.sokol200219@gmail.com>
2023-05-02 15:33:38 +0500

    я

--- Tabs/RoNight.js
+++ Tabs/RoNight.js
@@ -1,25 +1,118 @@
 document.write(`
-  <div class="card">
-  <div class="card-content">
-    <p class="title">
-     Яндекс Плюс
-    </p>
-    <p class="subtitle">
-      Кинопоиск, Музыка и кешбэк баллами в сервисах Яндекса
-    </p>
-  </div>
-  <footer class="card-footer">
-    <p class="card-footer-item">
-      <span>
-        Цена на момент поста: <r class="price"><strong>99</strong></r> ₽
-      </span>
-    </p>
-    <p class="card-footer-item">
-      <span>
-        Узнать цену сейчас: <a href="https://plus.yandex.ru/">PLUS</a>
-      </span>
-    </p>
-  </footer>
-</div>
-<p class="mb-4"></p>
+  <div class="card"><div class="card-content"><p class="title">
+
+  Аниме фигурка с Zero Two
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+   <div class="card"><div class="card-content"><p class="title">
+
+   Ulanzu Vl81 Свет
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+   <div class="card"><div class="card-content"><p class="title">
+
+   Пушистое худи XXXXXXXL размера, черное
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+     <div class="card"><div class="card-content"><p class="title">
+
+   Кружка 500мл
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+  <div class="card"><div class="card-content"><p class="title">
+
+   Острые чипсы:3
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+     <div class="card"><div class="card-content"><p class="title">
+
+   Подставка для кружки
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+     <div class="card"><div class="card-content"><p class="title">
+
+   Кейкапы PBT 
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+     <div class="card"><div class="card-content"><p class="title">
+
+   Подушки пушистые
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
+
+
+   <div class="card"><div class="card-content"><p class="title">
+
+  Повербанк
+  
+  </p> <p class="subtitle">
+
+  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+  
+  Купить: <a href="">нету</a>
+  
+  </span></p></footer></div><p class="mb-4"></p>
+
 `);
