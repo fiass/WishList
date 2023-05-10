196ed0caf04736a5aa62db04b230ae6109e3e9f5
Fiass <roma.sokol200219@gmail.com>
2023-05-10 10:06:23 +0500

    SpaceFix

--- Tabs/RoNight.js
+++ Tabs/RoNight.js
@@ -1,206 +1,192 @@
 document.write(`
-  <div class="card"><div class="card-content"><p class="title">
+ <div class="card"><div class="card-content"><p class="title">
 
-  Аниме фигурка с Zero Two
-  
-  </p> <p class="subtitle">
+ Аниме фигурка с Zero Two
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ </p> <p class="subtitle">
 
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
-   <div class="card"><div class="card-content"><p class="title">
+ Купить: <a href="">нету</a>
 
-   Ulanzu Vl81 Свет
-  
-  </p> <p class="subtitle">
+ </span></p></footer></div><p class="mb-4"></p>
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ <div class="card"><div class="card-content"><p class="title">
 
+ Ulanzu Vl81 Свет
 
-   <div class="card"><div class="card-content"><p class="title">
+ </p> <p class="subtitle">
 
-   Пушистое худи XXXXXXXL размера, черное
-  
-  </p> <p class="subtitle">
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ Купить: <a href="">нету</a>
 
-     <div class="card"><div class="card-content"><p class="title">
+ </span></p></footer></div><p class="mb-4"></p>
 
-   Кружка 500мл
-  
-  </p> <p class="subtitle">
+ <div class="card"><div class="card-content"><p class="title">
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ Пушистое худи XXXXXXXL размера, черное
 
+ </p> <p class="subtitle">
 
-  <div class="card"><div class="card-content"><p class="title">
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
-   Острые чипсы:3
-  
-  </p> <p class="subtitle">
+ Купить: <a href="">нету</a>
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ </span></p></footer></div><p class="mb-4"></p>
 
+ <div class="card"><div class="card-content"><p class="title">
 
-     <div class="card"><div class="card-content"><p class="title">
+ Кружка 500мл
 
-   Подставка для кружки
-  
-  </p> <p class="subtitle">
+ </p> <p class="subtitle">
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
+ Купить: <a href="">нету</a>
 
-     <div class="card"><div class="card-content"><p class="title">
+ </span></p></footer></div><p class="mb-4"></p>
 
-   Кейкапы PBT 
-  
-  </p> <p class="subtitle">
+ <div class="card"><div class="card-content"><p class="title">
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ Острые чипсы:3
 
+ </p> <p class="subtitle">
 
-     <div class="card"><div class="card-content"><p class="title">
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
-   Подушки пушистые
-  
-  </p> <p class="subtitle">
+ Купить: <a href="">нету</a>
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ </span></p></footer></div><p class="mb-4"></p>
 
+ <div class="card"><div class="card-content"><p class="title">
 
+ Подставка для кружки
 
-   <div class="card"><div class="card-content"><p class="title">
+ </p> <p class="subtitle">
 
-  Повербанк
-  
-  </p> <p class="subtitle">
-  
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ Купить: <a href="">нету</a>
 
+ </span></p></footer></div><p class="mb-4"></p>
 
-     <div class="card"><div class="card-content"><p class="title">
+ <div class="card"><div class="card-content"><p class="title">
 
-Миленький светильник конструктор
-  
-  </p>  
+ Кейкапы PBT 
+
+ </p> <p class="subtitle">
+
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+ Купить: <a href="">нету</a>
+
+ </span></p></footer></div><p class="mb-4"></p>
+
+ <div class="card"><div class="card-content"><p class="title">
+
+ Подушки пушистые
+
+ </p> <p class="subtitle">
+
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+ Купить: <a href="">нету</a>
+
+ </span></p></footer></div><p class="mb-4"></p>
+
+ <div class="card"><div class="card-content"><p class="title">
+
+ Повербанк
+
+ </p> <p class="subtitle">
+
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+ Купить: <a href="">нету</a>
+
+ </span></p></footer></div><p class="mb-4"></p>
+
+ <div class="card"><div class="card-content"><p class="title">
+
+ Миленький светильник конструктор
+
+ </p>  
+
  <p class="subtitle">
-не на мотоцикле
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="https://aliexpress.ru/item/1005004103791926.html?sub=1686862&af=739_1686862&utm_campaign=1686862&aff_platform=api-new-link-generate&sub1=1068989&utm_medium=cpa&cn=2ururu650vn8zb4fcyn27zqsy4r7c0xn&dp=2ururu650vn8zb4fcyn27zqsy4r7c0xn&aff_fcid=43eae5564c744c528258ed8726b43bfa-1683261680773-01885-_Ddre325&cv=3&aff_fsk=_Ddre325&sk=_Ddre325&aff_trace_key=43eae5564c744c528258ed8726b43bfa-1683261680773-01885-_Ddre325&terminal_id=69dcfa4898c84b799b4f0314c892eb99&utm_content=1068989&utm_source=admitad&sku_id=12000028673288999">Алиэкспресс</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ не на мотоцикле
 
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
-     <div class="card"><div class="card-content"><p class="title">
+ Купить: <a href="https://aliexpress.ru/item/1005004103791926.html?sub=1686862&af=739_1686862&utm_campaign=1686862&aff_platform=api-new-link-generate&sub1=1068989&utm_medium=cpa&cn=2ururu650vn8zb4fcyn27zqsy4r7c0xn&dp=2ururu650vn8zb4fcyn27zqsy4r7c0xn&aff_fcid=43eae5564c744c528258ed8726b43bfa-1683261680773-01885-_Ddre325&cv=3&aff_fsk=_Ddre325&sk=_Ddre325&aff_trace_key=43eae5564c744c528258ed8726b43bfa-1683261680773-01885-_Ddre325&terminal_id=69dcfa4898c84b799b4f0314c892eb99&utm_content=1068989&utm_source=admitad&sku_id=12000028673288999">Алиэкспресс</a>
 
-Пушистый коврик
-  
-  </p> <p class="subtitle">
+ </span></p></footer></div><p class="mb-4"></p>
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="https://aliexpress.ru/item/4001290992101.html?sku_id=12000018541336514&spm=a2g2w.productlist.search_results.4.66f34aa6wnFfyi">Алиэкпресс</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ <div class="card"><div class="card-content"><p class="title">
 
+ Пушистый коврик
 
-    <div class="card"><div class="card-content"><p class="title">
+ </p> <p class="subtitle">
 
-постельное белье
-  
-  </p><p class="subtitle">  Мягкое или шёлковое
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
- 
+ Купить: <a href="https://aliexpress.ru/item/4001290992101.html?sku_id=12000018541336514&spm=a2g2w.productlist.search_results.4.66f34aa6wnFfyi">Алиэкпресс</a>
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ </span></p></footer></div><p class="mb-4"></p>
 
+ <div class="card"><div class="card-content"><p class="title">
 
-   <div class="card"><div class="card-content"><p class="title">
+ постельное белье
 
-Коврик для мыши
-  
-  </p><p class="subtitle">
+ </p><p class="subtitle">  Мягкое или шёлковое
 
-  Размер: 900 мм x 400 мм 
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ Купить: <a href="">нету</a>
 
+ </span></p></footer></div><p class="mb-4"></p>
 
-   <div class="card"><div class="card-content"><p class="title">
+ <div class="card"><div class="card-content"><p class="title">
+
+ Коврик для мыши
+
+ </p><p class="subtitle">
+
+ Размер: 900 мм x 400 мм 
+
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+ Купить: <a href="">нету</a>
+
+ </span></p></footer></div><p class="mb-4"></p>
+
+ <div class="card"><div class="card-content"><p class="title">
 
  Данго (японская сладость)
-  
-  </p> <p class="subtitle">
-Данго - это небольшие (размером с орех) рисовые колобки, не имеющие вообще никакой начинки. Особенный вкус и небольшую сладость им придает специфический соус. Данго - блюдо с традицией. Его обычно подавали к столу в праздник любования луной – Цукими. Как правило, на блюдо выкладывали 15 шариков в виде пирамиды - это количество символизирует число месяца, в которое наступает самая лунная ночь.
+
+ </p> <p class="subtitle">
  
+ Данго - это небольшие (размером с орех) рисовые колобки, не имеющие вообще никакой начинки. Особенный вкус и небольшую сладость им придает специфический соус. Данго - блюдо с традицией. Его обычно подавали к столу в праздник любования луной – Цукими. Как правило, на блюдо выкладывали 15 шариков в виде пирамиды - это количество символизирует число месяца, в которое наступает самая лунная ночь.
  
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="">нету</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+ Купить: <a href="">нету</a>
+
+ </span></p></footer></div><p class="mb-4"></p>
 
 
 
-<div class="card"><div class="card-content"><p class="title">
+ <div class="card"><div class="card-content"><p class="title">
 
-Японский шоколад матча
-  
-  </p><p class="subtitle">
+ Японский шоколад матча
+
+ </p><p class="subtitle">
 
  я не знаю где его достать, пробывал только в детстве. 
-  </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
-  
-  Купить: <a href="https://roycechocolate.com/collections/matcha/products/nama-chocolate-matcha">roycechocolate</a>
-  
-  </span></p></footer></div><p class="mb-4"></p>
-`);
+ </p> </div><footer class="card-footer"><p class="card-footer-item"><span></span></p><p class="card-footer-item"><span>
+
+ Купить: <a href="https://roycechocolate.com/collections/matcha/products/nama-chocolate-matcha">roycechocolate</a>
+
+ </span></p></footer></div><p class="mb-4"></p>
+ `);
